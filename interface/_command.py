1a481dde2611830a84acbf6bc6d0cfe2b68f9c6a
DrDuckedGoose <40559528+DrDuckedGoose@users.noreply.github.com>
2025-09-24 16:14:19 +1000

    upload command - 24 9 25
    Adds the upload command

--- interface/_command.py
+++ interface/_command.py
@@ -24,6 +24,13 @@ class Command:
 #Example Command
 import requests
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
 class echo(Command):
 		name = "echo"
 		desc = "Example echo command."
@@ -31,7 +38,8 @@ class echo(Command):
 		async def run(self, message):
 			await Command.run(self, message)
 			#Echo text
-			await message.channel.send(message.content)
+			if(message.content):
+				await message.channel.send(message.content)
 			#Echo attachments
 			if(len(message.attachments) <= 0):
 				return
@@ -40,4 +48,68 @@ class echo(Command):
 			with open('attachment.jpg', 'wb') as handler: # This probably doesn't support more than one, ie naming issue
 				handler.write(img_data)
 
+#Upload command
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
+import os
+
+class upload(Command):
+		name = "upload"
+		desc = "Uploads attachments to the target drive in config."
+		# Our
+		service = None
+
+		def __init__(self, _server_id):
+			super().__init__(_server_id)
+			# Build our service connection shit to upload the files with
+			creds, _ = google.auth.default()
+			self.service = build("drive", "v3", credentials=creds)
+
+		async def run(self, message):
+			await Command.run(self, message)
+			#
+			if(len(message.attachments) <= 0):
+				await message.channel.send("No attachments found!")
+				return
+			#
+			location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+			#
+			for attachment in message.attachments:
+				# Get a file name
+				filename = attachment.url.split('/')[-1]
+				filename = filename.split('.')[0]
+				# Figure out content types, for file extensions
+				file_extension  = attachment.content_type
+				match file_extension:
+					case "application/pdf":
+						file_extension = ".pdf"
+					case _:
+						await message.channel.send("Unsupported file type!")
+						return
+				# Get the attachment data
+				img_data = requests.get(attachment.url).content
+				location = os.path.join(location, filename+file_extension)
+				# Save the attachment to disk
+				with open(location, 'wb') as handler:
+					handler.write(img_data)
+				# Upload attachment
+				try:
+					# create drive api client
+					file_metadata = {"name": filename+file_extension}
+					media = MediaFileUpload(location, mimetype=attachment.content_type)
+					# pylint: disable=maybe-no-member
+					file = (
+						self.service.files()
+						.create(body=file_metadata, media_body=media, fields="id")
+						.execute()
+					)
+					print(f'File ID: {file.get("id")}')
+					# Delete attachment
+					os.remove(location)
+				except HttpError as error:
+					print(f"An error occurred: {error}")
+					file = None
 
